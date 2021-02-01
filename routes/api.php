091f964267a05b6a3ece42484366ad4b9d85e4a7
sexonsonic <farrelsanan12@gmail.com>
2021-02-01 16:54:53 +0700

    Rest API Provinsi

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Provinsi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProvinsiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API Provinsi
+Route::get('provinsi', [ProvinsiController::class, 'index']);
+Route::post('provinsi', [ProvinsiController::class, 'store']);
+Route::get('provinsi/{id}', [ProvinsiController::class, 'show']);
+Route::post('provinsi/update/{id}', [ProvinsiController::class, 'update']);
+Route::delete('/provinsi/{id}', [ProvinsiController::class, 'destroy']);
